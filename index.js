cc1ca5de27e25f05be44a7bf3f149e0ffc132049
willis plummer <willisplummer@gmail.com>
2018-01-20 07:07:06 -0500

    round squares, random colors

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-import Rx from 'rxjs/Rx';
+import Rx from 'rxjs/Rx'
 
 const SQUARE_IDS = ["square-1", "square-2", "square-3", "square-4", "square-5", "square-6", "square-7", "square-8", "square-9", "square-10", "square-11", "square-12"]
 
-const COLORS = ["teal", "red", "green", "blue", "orange"]
+const COLORS = ["teal", "red", "green", "blue", "orange", "yellow"]
 
-const squareMouseOverStreams = SQUARE_IDS.map(id =>
+const squareMouseOver$ = SQUARE_IDS.map(id =>
   Rx.Observable.fromEvent(document.getElementById(`${id}`), 'mouseover')
     .map(e => id)
 )
 
-const mergedSquareStreams = Rx.Observable.merge(...squareMouseOverStreams)
+const mergedSquare$ = Rx.Observable.merge(...squareMouseOver$)
 
 const setBackgroundColor = id => {
   const el = document.getElementById(id)
   const backgroundColor = el.style.backgroundColor || COLORS[0]
-  const newColor = COLORS.concat(COLORS)[COLORS.findIndex(e => e === backgroundColor) + 1]
-  el.setAttribute("style", `background-color: ${newColor};`);
+  const otherColors = COLORS.filter(c => c !== backgroundColor)
+  // side-effect: ideally would inject the seed to keep this function pure
+  const newColor = otherColors[Math.floor(Math.random()*otherColors.length)]
+  el.setAttribute("style", `background-color: ${newColor};`)
 }
 
-mergedSquareStreams.subscribe(setBackgroundColor)
+mergedSquare$.subscribe(setBackgroundColor)
