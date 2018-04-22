afed07b4b9ba259a9c6c1e42edf9f461f3366827
willis plummer <willisplummer@gmail.com>
2018-04-22 12:14:33 -0400

    replace rxjs with callbags

--- squares.js
+++ squares.js
@@ -1,4 +1,4 @@
-import Rx from 'rxjs/Rx'
+const {forEach, fromEvent, map, filter, pipe, merge} = require('callbag-basics');
 
 const COLORS = ['#a8e6cf', '#dcedc1', '#ffd3b6', '#ffaaa5', '#ff8b94']
 const SQUARE_COUNT = 50
@@ -38,14 +38,6 @@ addRows(squares, rowsToAdd)
 const squareIds = Array(SQUARE_COUNT).fill(undefined)
   .map((_, i) => `square-${i + 1}`)
 
-
-const squareMouseOver$ = squareIds.map(id =>
-  Rx.Observable.fromEvent(document.getElementById(`${id}`), 'mouseover')
-    .map(e => id)
-)
-
-const mergedSquare$ = Rx.Observable.merge(...squareMouseOver$)
-
 const rgbToHex = (rgb) => {
   if (/^#[0-9A-F]{6}$/i.test(rgb)) return rgb;
 
@@ -68,4 +60,10 @@ const setBackgroundColor = id => {
   el.style.backgroundColor = newColor;
 }
 
-mergedSquare$.subscribe(setBackgroundColor)
+pipe(
+  fromEvent(document, 'mouseover'),
+  filter(ev => squareIds.includes(ev.target.id)),
+  map(ev => ev.target.id),
+  forEach(setBackgroundColor)
+)
+
